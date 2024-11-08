24167e66d02739a4d8988d452e2c0cf77e4f79ed
bukhari06 <rukhjabbar@gmail.com>
2024-11-08 17:50:34 +0500

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ const HomePage = () => {
       𝐌𝐀𝐍𝐙𝐙𝐀𝐑𝐈        
       </div>
 
-      <h1 className="w-[496px] h-[189px] absolute top-[316px] left-[250px] font-bold text-[35px] leading-[55.7px] tracking-[2.5%] text-[#000000] ">
+      <h1 className="w-[496px] h-[189px] absolute top-[316px] left-[220px] font-bold text-[35px] leading-[55.7px] tracking-[2.5%] text-[#000000] ">
       𝕀𝕄ℙ𝔼ℂℂ𝔸𝔹𝕃𝔼 ℂℝ𝔸𝔽𝕋𝕊𝕄𝔸ℕ𝕊ℍ𝕀ℙ 𝔸ℕ𝔻 𝔽𝕀ℕ𝔼𝕊𝕊𝔼</h1>
 
-      <p className=" w-[902px] h-[147px] absolute top-[533px] left-[250px] font-serif font-medium text-[30px] leading-[49.35px] tracking-[2.5%] text-[#787054]   ">
+      <p className=" w-[902px] h-[147px] absolute top-[533px] left-[220px]  text-[28px] leading-[49.35px] tracking-[2.5%] text-[#787054]   ">
       𝘈𝘯 𝘦𝘹𝘢𝘮𝘱𝘭𝘦 𝘰𝘧 𝘪𝘯𝘵𝘳𝘪𝘤𝘢𝘵𝘦 𝘸𝘰𝘳𝘬𝘮𝘢𝘯𝘴𝘩𝘪𝘱 𝘢𝘯𝘥 𝘥𝘦𝘵𝘢𝘪𝘭, 𝘦𝘭𝘦𝘨𝘢𝘯𝘵 <br /> 𝘯𝘦𝘤𝘬𝘭𝘢𝘤𝘦𝘴 𝘢𝘯𝘥 𝘭𝘰𝘯𝘨 𝘢𝘯𝘥 𝘴𝘩𝘰𝘳𝘵 𝘤𝘩𝘢𝘪𝘯𝘴 𝘧𝘰𝘳𝘮 𝘢 𝘱𝘢𝘳𝘵 𝘰𝘧<br />𝘰𝘶𝘳 𝘥𝘦𝘴𝘪𝘳𝘢𝘣𝘭𝘦 𝘤𝘰𝘭𝘭𝘦𝘤𝘵𝘪𝘰𝘯.
       </p>
 
-      <button className="w-[270px] h-[58px] absolute top-[730px] left-[250px] font-serif font-medium text-[25px] leading-[37.5px] bg-[#A29875] text-[#FFFFFF]  p-[10px] gap-[10px] rounded-[10px] ">
+      <button className="w-[270px] h-[58px] absolute top-[730px] left-[220px]  text-[25px] leading-[37.5px] bg-[#A29875] text-[#FFFFFF]  p-[10px] gap-[10px] rounded-[10px] ">
       ᴇxᴘʟᴏʀᴇ ɴᴏᴡ
       </button>
      <div className="absolute top-[198px] left-[1000px] ">
